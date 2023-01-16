9b7547f7b664b8d321d122b2935496c7d95f6721
nidheesh <nidhin@Dilips-iMac.local>
2023-01-16 12:33:18 +0530

    override

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,21 +1,21 @@
 package com.driver;
 
-
-class A{
-    String meth(){
-        return "Invoking method from class A" ;
-    }
-}
-class B extends A{
-
-    String meth() {
-        return "Method is overridden in Extendend class B";
-    }
-
-}
 public class Main {
+    static class A {
+        String meth() {
+            return "Invoking method from class A";
+        }
+    }
+
+    static class B extends A {
+
+        String meth() {
+            return "Method is overridden in Extendend class B";
+        }
+    }
+
     public static void main(String[] arg) {
-        B obj1=new B();
+        B obj1 = new B();
         System.out.println(obj1.meth());
         B obj = new B();
         System.out.println(obj.meth());
